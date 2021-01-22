497d929d16551f2e277c1c7b7598142580aad415
Julieta Azzini <juliazzini1@gmail.com>
2021-01-22 17:11:07 -0300

    making cards carousel

--- src/components/InnerCarousel/index.js
+++ src/components/InnerCarousel/index.js
@@ -1,22 +1,31 @@
 import React from "react";
 import "../InnerCarousel/style.scss";
+import Slider from "react-slick";
 
 function InnerCarousel(props) {
+  const settings = {
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    variableHeight: true,
+  };
   return (
     <div className="products-wrapper">
-      {props.filter.map((product) => {
-        return (
-          <div className="product-wrapper active">
-            <div className="image-container">
-              <img src={product.img} alt="image" className="product-image" />
+      <Slider {...settings}>
+        {props.filter.map((product) => {
+          return (
+            <div className="product-wrapper active">
+              <div className="image-container">
+                <img src={product.img} alt="image" className="product-image" />
+              </div>
+              <div className="product-description">
+                <span className="product-price">$ {product.price}</span>
+                <span className="product-name">{product.name}</span>
+              </div>
             </div>
-            <div className="product-description">
-              <span className="product-price">$ {product.price}</span>
-              <span className="product-name">{product.name}</span>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </Slider>
     </div>
   );
 }
