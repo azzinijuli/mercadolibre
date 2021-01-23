f007b876b4dca0ea87c5b788d02950f64d50fc79
Julieta Azzini <juliazzini1@gmail.com>
2021-01-23 17:33:45 -0300

    added new carousel

---
+++ src/components/Card/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../Card/style.scss";
+
+function Card(props) {
+  const product = props.product;
+  return (
+    <div className="product-container">
+      <div className="image-container">
+        <img
+          src={product.img}
+          alt="product thumbnail"
+          className="product-image"
+        />
+      </div>
+      <div className="product-description">
+        <span className="product-price">$ {product.price}</span>
+        <span className="product-name">{product.name}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/components/InnerCarousel/index.js
+++ src/components/InnerCarousel/index.js
@@ -1,22 +1,50 @@
 import React from "react";
+import Card from "../Card";
 import "../InnerCarousel/style.scss";
+import Slider from "react-slick";
 
 function InnerCarousel(props) {
+  const filter = props.filter;
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div className="products-wrapper">
-      {props.filter.map((product) => {
-        return (
-          <div className="product-wrapper active">
-            <div className="image-container">
-              <img src={product.img} alt="image" className="product-image" />
+    <div className="inner-carousel">
+      <Slider {...settings}>
+        {filter.map((product, key) => {
+          return (
+            <div className="product-wrapper" key={key}>
+              <Card product={product} />
             </div>
-            <div className="product-description">
-              <span className="product-price">$ {product.price}</span>
-              <span className="product-name">{product.name}</span>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </Slider>
     </div>
   );
 }
